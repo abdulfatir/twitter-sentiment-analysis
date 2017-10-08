7f5621c1bcb16300ba1c722a923e51eff65cf9bc
abdulfatir <abdulfatirs@gmail.com>
2017-10-09 05:20:06 +0800

    Added preprocessing

---
+++ preprocess.py
@@ -0,0 +1,91 @@
+import re
+import sys
+
+
+def preprocess_word(word):
+    # Remove punctuation
+    word = word.strip('\'"?!,.():;')
+    # Convert more than 2 letter repetitions to 2 letter
+    # funnnnny --> funny
+    word = re.sub(r'(.)\1+', r'\1\1', word)
+    # Remove - & '
+    word = re.sub(r'(-|\')', '', word)
+    return word
+
+
+def is_valid_word(word):
+    # Check if word begins with an alphabet
+    return (re.search(r'^[a-zA-Z][a-z0-9A-Z\._]*$', word) is not None)
+
+
+def handle_emojis(tweet):
+    # Smile -- :), : ), :-), (:, ( :, (-:, :')
+    tweet = re.sub(r'(:\s?\)|:-\)|\(\s?:|\(-:|:\'\))', 'EMO_POS', tweet)
+    # Laugh -- :D, : D, :-D, xD, x-D, XD, X-D
+    tweet = re.sub(r'(:\s?D|:-D|x-?D|X-?D)', 'EMO_POS', tweet)
+    # Love -- <3, :*
+    tweet = re.sub(r'(<3|:\*)', 'EMO_POS', tweet)
+    # Wink -- ;-), ;), ;-D, ;D, (;,  (-;
+    tweet = re.sub(r'(;-?\)|;-?D|\(-?;)', 'EMO_POS', tweet)
+    # Sad -- :-(, : (, :(, ):, )-:
+    tweet = re.sub(r'(:\s?\(|:-\(|\)\s?:|\)-:)', 'EMO_NEG', tweet)
+    # Cry -- :,(, :'(, :"(
+    tweet = re.sub(r'(:,\(|:\'\(|:"\()', 'EMO_NEG', tweet)
+    return tweet
+
+
+def preprocess_tweet(tweet):
+    processed_tweet = []
+    # Convert to lower case
+    tweet = tweet.lower()
+    # Replaces URLs with the word URL
+    tweet = re.sub(r'((www\.[\S]+)|(https?://[\S]+))', 'URL', tweet)
+    # Replace @handle with the word USER_MENTION
+    tweet = re.sub(r'@[\S]+', 'USER_MENTION', tweet)
+    # Replace multiple spaces with a single space
+    tweet = re.sub(r'\s+', ' ', tweet)
+    # Replaces #hashtag with hashtag
+    tweet = re.sub(r'#(\S+)', r'\1', tweet)
+    # Remove RT (retweet)
+    tweet = re.sub(r'\brt\b', '', tweet)
+    # Replace 2+ dots with space
+    tweet = re.sub(r'\.{2,}', ' ', tweet)
+    # Strip space, " and ' from tweet
+    tweet = tweet.strip(' "\'')
+    # Replace emojis with either EMO_POS or EMO_NEG
+    tweet = handle_emojis(tweet)
+    words = tweet.split(' ')
+
+    for word in words:
+        word = preprocess_word(word)
+        if is_valid_word(word):
+            processed_tweet.append(word)
+
+    return ' '.join(processed_tweet)
+
+
+def write_status(i, total):
+    sys.stdout.write('\r')
+    sys.stdout.write('Processing tweet %d/%d' % (i, total))
+    sys.stdout.flush()
+
+
+if __name__ == '__main__':
+    csv_file_name = sys.argv[1]
+    save_to_file = open(sys.argv[2], 'w')
+
+    with open(csv_file_name, 'r') as csv:
+        lines = csv.readlines()
+        total = len(lines)
+        for i, line in enumerate(lines):
+            tweet_id = line[:line.find(',')]
+            line = line[1 + line.find(','):]
+            positive = int(line[:line.find(',')])
+            line = line[1 + line.find(','):]
+            tweet = line
+            processed_tweet = preprocess_tweet(tweet)
+            save_to_file.write('%s,%d,%s\n' %
+                               (tweet_id, positive, processed_tweet))
+            if i % 1000 == 0:
+                write_status(i, total)
+    save_to_file.close()
