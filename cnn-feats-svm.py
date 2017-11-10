37e431d4736b9e0c88ce4d832fc4a9154f4cb68b
abdulfatir <abdulfatirs@gmail.com>
2017-11-10 23:02:55 +0800

    Added CNN

---
+++ cnn-feats-svm.py
@@ -0,0 +1,46 @@
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+from numpy import loadtxt
+import numpy as np
+import pickle
+import utils
+
+TRAIN_FEATURES_FILE = './train-feats.npy'
+TRAIN_LABELS_FILE = './train-labels.txt'
+TEST_FEATURES_FILE = './test-feats.npy'
+CLASSIFIER = 'SVM'
+MODEL_FILE = 'cnn-feats-%s.pkl' % CLASSIFIER
+TRAIN = True
+C = 10
+MAX_ITER = 1000
+
+if TRAIN:
+    X_train = np.load(TRAIN_FEATURES_FILE)
+    y_train = loadtxt(TRAIN_LABELS_FILE, dtype=float).astype(int)
+
+    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.1)
+
+    print X_train.shape, y_train.shape, X_val.shape, y_val.shape
+
+    if CLASSIFIER == 'SVM':
+        model = svm.LinearSVC(C=C, verbose=1, max_iter=MAX_ITER)
+        model.fit(X_train, y_train)
+
+    print model
+    del X_train
+    del y_train
+    with open(MODEL_FILE, 'wb') as mf:
+        pickle.dump(model, mf)
+    val_preds = model.predict(X_val)
+    accuracy = accuracy_score(y_val, val_preds)
+    print("Val Accuracy: %.2f%%" % (accuracy * 100.0))
+
+else:
+    with open(MODEL_FILE, 'rb') as mf:
+        model = pickle.load(mf)
+    X_test = np.load(TEST_FEATURES_FILE)
+    print X_test.shape
+    test_preds = model.predict(X_test)
+    results = zip(map(str, range(X_test.shape[0])), test_preds)
+    utils.save_results_to_csv(results, 'cnn-feats-svm-linear-%.2f-%d.csv' % (C, MAX_ITER))
---
+++ cnn.py
@@ -0,0 +1,132 @@
+import numpy as np
+import sys
+from keras.models import Sequential, load_model
+from keras.layers import Dense, Dropout, Activation
+from keras.layers import Embedding, Flatten
+from keras.layers import Conv1D, GlobalMaxPooling1D
+from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
+from keras.optimizers import SGD
+import utils
+from keras.preprocessing.sequence import pad_sequences
+
+FREQ_DIST_FILE = '../train-processed-freqdist.pkl'
+BI_FREQ_DIST_FILE = '../train-processed-freqdist-bi.pkl'
+TRAIN_PROCESSED_FILE = '../train-processed.csv'
+TEST_PROCESSED_FILE = '../test-processed.csv'
+GLOVE_FILE = './dataset/glove-seeds.txt'
+dim = 200
+
+
+def get_glove_vectors(vocab):
+    print 'Looking for GLOVE seeds'
+    glove_vectors = {}
+    found = 0
+    with open(GLOVE_FILE, 'r') as glove_file:
+        for i, line in enumerate(glove_file):
+            utils.write_status(i + 1, 0)
+            tokens = line.strip().split()
+            word = tokens[0]
+            if vocab.get(word):
+                vector = [float(e) for e in tokens[1:]]
+                glove_vectors[word] = np.array(vector)
+                found += 1
+    print '\n'
+    return glove_vectors
+
+
+def get_feature_vector(tweet):
+    words = tweet.split()
+    feature_vector = []
+    bi_feature_vector = []
+    for i in range(len(words) - 1):
+        word = words[i]
+        if vocab.get(word) is not None:
+            feature_vector.append(vocab.get(word))
+        if add_bigrams:
+            next_word = words[i + 1]
+            if bigram_vocab.get((word, next_word)) is not None:
+                bi_feature_vector.append(bigram_vocab.get((word, next_word)))
+    if len(words) >= 1:
+        if vocab.get(words[-1]) is not None:
+            feature_vector.append(vocab.get(words[-1]))
+    feature_vector = feature_vector + bi_feature_vector
+    return feature_vector
+
+
+def process_tweets(csv_file, test_file=True):
+    tweets = []
+    labels = []
+    print 'Generating feature vectors'
+    with open(csv_file, 'r') as csv:
+        lines = csv.readlines()
+        total = len(lines)
+        for i, line in enumerate(lines):
+            if test_file:
+                tweet_id, tweet = line.split(',')
+            else:
+                tweet_id, sentiment, tweet = line.split(',')
+            feature_vector = get_feature_vector(tweet)
+            if test_file:
+                tweets.append(feature_vector)
+            else:
+                tweets.append(feature_vector)
+                labels.append(int(sentiment))
+            utils.write_status(i + 1, total)
+    print '\n'
+    return tweets, np.array(labels)
+
+
+if __name__ == '__main__':
+    train = len(sys.argv) == 1
+    np.random.seed(1337)
+    vocab_size = 90000
+    bigram_size = 20000
+    batch_size = 500
+    max_length = 40
+    filters = 600
+    kernel_size = 3
+    add_bigrams = False
+    vocab = utils.top_n_words(FREQ_DIST_FILE, vocab_size, shift=1)
+    glove_vectors = get_glove_vectors(vocab)
+    if add_bigrams:
+        bigram_vocab = utils.top_n_bigrams(BI_FREQ_DIST_FILE, bigram_size, shift=(vocab_size + 1))
+        vocab_size += bigram_size
+        max_length = 30
+    tweets, labels = process_tweets(TRAIN_PROCESSED_FILE, test_file=False)
+    embedding_matrix = np.random.randn(vocab_size + 1, dim) * 0.01
+    for word, i in vocab.items():
+        glove_vector = glove_vectors.get(word)
+        if glove_vector is not None:
+            embedding_matrix[i] = glove_vector
+    tweets = pad_sequences(tweets, maxlen=max_length, padding='post')
+    shuffled_indices = np.random.permutation(tweets.shape[0])
+    tweets = tweets[shuffled_indices]
+    labels = labels[shuffled_indices]
+    if train:
+        model = Sequential()
+        model.add(Embedding(vocab_size + 1, dim, weights=[embedding_matrix], input_length=max_length))
+        model.add(Dropout(0.4))
+        model.add(Conv1D(filters, kernel_size, padding='valid', activation='relu', strides=1))
+        model.add(Conv1D(300, kernel_size, padding='valid', activation='relu', strides=1))
+        model.add(Conv1D(150, kernel_size, padding='valid', activation='relu', strides=1))
+        model.add(Conv1D(75, kernel_size, padding='valid', activation='relu', strides=1))
+        model.add(Flatten())
+        model.add(Dense(600))
+        model.add(Dropout(0.5))
+        model.add(Activation('relu'))
+        model.add(Dense(1))
+        model.add(Activation('sigmoid'))
+        sgd = SGD(lr=1e-3, decay=1e-6, momentum=0.9, nesterov=True)
+        model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+        filepath = "./models/4cnn-{epoch:02d}-{loss:0.3f}-{acc:0.3f}-{val_loss:0.3f}-{val_acc:0.3f}.hdf5"
+        checkpoint = ModelCheckpoint(filepath, monitor="loss", verbose=1, save_best_only=True, mode='min')
+        reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=2, min_lr=0.000001)
+        model.fit(tweets, labels, batch_size=128, epochs=8, validation_split=0.1, shuffle=True, callbacks=[checkpoint, reduce_lr])
+    else:
+        model = load_model(sys.argv[1])
+        print model.summary()
+        test_tweets, _ = process_tweets(TEST_PROCESSED_FILE, test_file=True)
+        test_tweets = pad_sequences(test_tweets, maxlen=max_length, padding='post')
+        predictions = model.predict(test_tweets, batch_size=128, verbose=1)
+        results = zip(map(str, range(len(test_tweets))), np.round(predictions[:, 0]).astype(int))
+        utils.save_results_to_csv(results, 'glove-cnn-%d-%d-%d.csv' % (vocab_size, max_length, filters))
